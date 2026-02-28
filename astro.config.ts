f82914d4d7bb41bf6e024a3d2955e11a4350c73e
Patrick M <johndoe@example.com>
2026-02-28 10:13:05 -0500

    code color

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
       // For more themes, visit https://shiki.style/themes
-      themes: { light: "github-light", dark: "github-dark-default" },
+      themes: { light: "one-light", dark: "dark-plus" },
       defaultColor: false,
       wrap: false,
       transformers: [
